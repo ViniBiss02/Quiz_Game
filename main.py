34e1e574601bd9dfbca83a63f0cce96181b1b9d7
ViniBiss02 <viniciusbiss02@gmail.com>
2025-11-27 20:56:07 -0300

    adiciona perguntas interativas ao jogo de trivia

--- main.py
+++ main.py
@@ -12,9 +12,59 @@ while True:
 name = input("Qual é o seu nome? ").strip()
 print(f"Ótimo, {name}! Vamos começar o jogo.\n")
 
-answer = input("Qual é a capital da França? ").strip().lower()
-if answer == "paris":
-    print("Correto! Paris é a capital da França.")
-else:
-    print("Incorreto. A resposta correta é Paris.")
+while True:
+    answer = input("Qual é a capital da França? ").strip().lower()
+    if answer.lower() == "paris":
+        print("Correto! Paris é a capital da França.\n")
+        break
+    else:
+        print("Incorreto. A resposta correta é Paris.")
+        raise SystemExit
 
+while True:
+    answer_1 = input("Qual é 5 + 7? ").strip()
+    if answer_1 == "12":
+        print("Correto! 5 + 7 é 12.\n")
+        break
+    else:
+        print("Incorreto. A resposta correta é 12.")
+        raise SystemExit
+
+while True:
+    answer_2 = input("complete a frase:\nHoje mais cedo, eu escutei mamonas _______ e fiquei feliz\n"
+                     "(1) mortas\n"
+                     "(2) assassinas\n"
+                     "(3) alegres\n"
+                     "(4) do rei\n").strip().lower()
+    match answer_2:
+        case "1" | "mortas":
+            print("Incorreto. A resposta correta é 'assassinas'.")
+            raise SystemExit
+        case "2" | "assassinas":
+            print("Correto! A frase completa é: Hoje mais cedo, eu escutei mamonas assassinas e fiquei feliz.\n")
+            break
+        case "3" | "alegres":
+            print("Incorreto. A resposta correta é 'assassinas'.")
+            raise SystemExit
+        case "4" | "do rei":
+            print("Incorreto. A resposta correta é 'assassinas'.")
+            raise SystemExit
+        case _:
+            print("Resposta inválida. Por favor, escolha uma das opções fornecidas.")
+
+while True:
+    answer_3 = input("Qual é o maior planeta do nosso sistema solar? ").strip().lower()
+    if answer_3.lower() in ("júpiter", "jupiter"):
+        print("Correto! Júpiter é o maior planeta do nosso sistema solar.\n")
+        break
+    else:
+        print("Incorreto. A resposta correta é Júpiter.")
+        raise SystemExit
+
+print(f"Parabéns, {name}! Você respondeu todas as perguntas corretamente e venceu o jogo!\n\n")
+playagain = input("Você quer jogar novamente? (sim/não): ").strip().lower()
+if playagain in ("sim", "s"):
+    print("\nReiniciando o jogo...\n")
+    import main  # Reinicia o jogo importando o próprio arquivo
+else:
+    print("Obrigado por jogar! Até a próxima!")
